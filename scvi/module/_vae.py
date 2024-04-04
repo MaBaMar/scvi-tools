Fixing ll calculation

scvi/module/_vae.py
@@ -670,14 +670,13 @@ class VAE(BaseMinifiedModeModuleClass):
 
             # Log-probabilities
             """Prior distribution p(z), variational posterior q(z)"""
-            p_z = (
-                self.prior.log_prob(z).sum(dim=-1)
-            )
+            p_z = self.prior.log_prob(z)
+
             p_x_zl = -reconst_loss
             q_z_x = qz.log_prob(z).sum(dim=-1)
             log_prob_sum = p_z + p_x_zl - q_z_x  # log(p(z) * p(x|zl) * q(z|x))
 
-            pz_sum.append(p_z.item())
+            pz_sum.append(p_z)
             p_x_zl_sum.append(p_x_zl)
             q_z_x_sum.append(q_z_x)
 
@@ -698,18 +697,20 @@ class VAE(BaseMinifiedModeModuleClass):
 
             to_sum.append(log_prob_sum)
         to_sum = torch.cat(to_sum, dim=0)
+
+        pz_sum = logsumexp(torch.cat(pz_sum, dim=0), dim=0)
         p_x_zl_sum = logsumexp(torch.cat(p_x_zl_sum, dim=0), dim=0)
         q_z_x_sum = logsumexp(torch.cat(q_z_x_sum, dim=0), dim=0)
 
         batch_log_lkl = logsumexp(to_sum, dim=0) - np.log(n_mc_samples)
         if return_mean:
             batch_log_lkl = torch.mean(batch_log_lkl).item()
-            pz_sum = np.array(pz_sum).mean()
+            pz_sum = torch.mean(pz_sum).item()
             p_x_zl_sum = torch.mean(p_x_zl_sum).item()
             q_z_x_sum = torch.mean(q_z_x_sum).item()
         else:
             batch_log_lkl = batch_log_lkl.cpu()
-            pz_sum = pz_sum # TODO: fix
+            pz_sum = pz_sum.cpu()
             p_x_zl_sum = p_x_zl_sum.cpu()
             q_z_x_sum = q_z_x_sum.cpu()
         return batch_log_lkl, pz_sum, p_x_zl_sum, q_z_x_sum