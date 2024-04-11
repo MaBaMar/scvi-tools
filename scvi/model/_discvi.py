add prediction capability and fix SCVI KL-Scaling bug

scvi/model/_discvi.py
@@ -224,6 +224,28 @@ class DiSCVI(RNASeqMixin, VAEMixin, UnsupervisedTrainingMixin, BaseModelClass):
         """
         latent = self.get_latent_representation()
         n_d = self._module_kwargs["n_latent_d"]
-        n_x = self._module_kwargs["n_latent_x"]+n_d
-        n_y = self._module_kwargs["n_latent_y"]+n_x
+        n_x = self._module_kwargs["n_latent_x"] + n_d
+        n_y = self._module_kwargs["n_latent_y"] + n_x
         return latent[:, :n_d], latent[:, n_d:n_x], latent[:, n_x:n_y]
+
+    @torch.inference_mode()
+    def predict(
+        self, adata: Optional[AnnData] = None,
+        indices: Optional[Sequence[int]] = None,
+        batch_size: Optional[int] = None,
+    ):
+        """
+        :returns: (y, y_hat)
+        """
+        predictions = []
+        labels = []
+        self._check_if_trained(warn=False)
+        adata = self._validate_anndata(adata)
+        scdl = self._make_data_loader(adata=adata, indices=indices, batch_size=batch_size)
+
+        for tensors in scdl:
+            model_pred = self.module.predict(tensors)
+            predictions.append(model_pred[0].cpu())
+            labels.append(model_pred[1].cpu())
+
+        return torch.cat(labels, dim=0), torch.cat(predictions, dim=0)

scvi/module/_diva.py
@@ -1,7 +1,7 @@
 """DIVA implementation supervised"""
 from __future__ import annotations
 
-from typing import Literal, Optional
+from typing import Literal, Optional, Tuple
 
 import numpy as np
 import torch
@@ -433,3 +433,21 @@ class DIVA(BaseModuleClass):
     def sample(self, *args, **kwargs):
         # not really needed for our experiments
         raise NotImplementedError
+
+    @auto_move_data
+    def predict(self, tensors) -> Tuple[torch.Tensor, torch.Tensor]:
+        """
+        :param tensors: Input tensors of batch as provided by dataloader
+        :return: Tuple of (y_hat, y) where y_hat is the prediction and y the ground truth
+        """
+        x = tensors[REGISTRY_KEYS.X_KEY]
+
+        if self.log_variational:
+            x_ = torch.log(1 + x)
+        else:
+            x_ = x
+
+        _, zy_x = self.posterior_zy_x_encoder(x_)
+        _, y_hat = self.aux_y_enc(zy_x).max(dim=1)
+        y = tensors[REGISTRY_KEYS.LABELS_KEY]
+        return y_hat, y