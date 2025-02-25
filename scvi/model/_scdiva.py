adding: 1. any-covariate priors 2. softmax to celltype predictor during training fixing: 1. remove redundant prior component

scvi/model/_scdiva.py
@@ -11,7 +11,7 @@ from scvi._types import Tunable
 from scvi.data import AnnDataManager
 from scvi.data._utils import _check_if_view, get_anndata_attribute
 from scvi.data.fields import LabelsWithUnlabeledObsField, NumericalObsField, LayerField, CategoricalObsField
-from scvi.dataloaders._data_splitting import DefaultDataSplitter, SemiSupervisedDataSplitter
+from scvi.dataloaders._data_splitting import DefaultDataSplitter
 from scvi.model._utils import _init_library_size, get_max_epochs_heuristic, use_distributed_sampler
 from scvi.model.base import RNASeqMixin, VAEMixin, BaseModelClass, UnsupervisedTrainingMixin
 from scvi.module import DIVA
@@ -419,12 +419,6 @@ class SCDIVA(RNASeqMixin, VAEMixin, UnsupervisedTrainingMixin, BaseModelClass):
 
         return adata
 
-    def y_prior_logprob(self, tensors: torch.Tensor, mode: Literal['max', 'mean'] = 'mean') -> torch.Tensor:
-        if mode == 'mean':
-            return self.module.full_y_prior_dist().log_prob(tensors.to(self.module.device)).cpu().numpy()
-        else:
-            return self.draw_from_all_priors(tensors).max(axis=0)
-
     @torch.inference_mode()
     @auto_move_data
     def draw_from_all_priors(self, tensors: torch.Tensor) -> np.ndarray:
@@ -492,6 +486,7 @@ class SCDIVA(RNASeqMixin, VAEMixin, UnsupervisedTrainingMixin, BaseModelClass):
                 submask[np.argpartition(candidates, -n_samples)[-n_samples:]] = True
                 index_mask[mask] = submask
         return index_mask
+
 
 class TunedSCDIVA(VAEMixin, UnsupervisedTrainingMixin, BaseModelClass):
     _module_cls = TunedDIVA

scvi/model/_scdivarqm.py
@@ -5,17 +5,16 @@ from typing import Literal, Union, Callable, Optional, Sequence
 import scvi.nn
 import torch
 from anndata import AnnData
-from scvi.dataloaders._data_splitting import MixedRatioDataSplitter
 from scvi import settings
 from scvi.data._constants import _MODEL_NAME_KEY, _SETUP_ARGS_KEY, _SCVI_VERSION_KEY
 from scvi.dataloaders import SemiSupervisedDataLoader
+from scvi.dataloaders._data_splitting import MixedRatioDataSplitter
 from scvi.model import SCDIVA
 from scvi.model._utils import parse_device_args
 from scvi.model.base import ArchesMixin, BaseModelClass
 from scvi.model.base._archesmixin import _get_loaded_data
 from scvi.model.base._utils import _validate_var_names, _initialize_model
 from scvi.module._diva_rqm import RQMDiva
-from scvi.nn._base_components import FCLayers
 from scvi.utils._docstrings import devices_dsp
 from tqdm import tqdm
 
@@ -117,6 +116,7 @@ class ScDiVarQM(SCDIVA, ArchesMixin):
             **registry[_SETUP_ARGS_KEY]
         )
         attr_dict['init_params_']['kwargs']['kwargs'].update({'use_ratio_data_splitter': use_ratio_data_splitter})
+        # attr_dict['init_params_']['kwargs']['kwargs'].update({'beta_y': 1})
         model: ScDiVarQM = _initialize_model(cls, adata, attr_dict)
         adata_manager = model.get_anndata_manager(adata, required=True)
         version_split = adata_manager.registry[_SCVI_VERSION_KEY].split(".")
@@ -207,6 +207,7 @@ class ScDiVarQM(SCDIVA, ArchesMixin):
                 "Please raise an issue on github if you need it."
             )
 
+
 def _process_layer(layer: torch.nn.Sequential, freeze_grads: bool, freeze_batchnorm: bool, freeze_dropout: bool):
     layer_components: list[torch.nn.Module] = [*layer.children()]
     layer_components[0].requires_grad_(not freeze_grads)
@@ -215,7 +216,8 @@ def _process_layer(layer: torch.nn.Sequential, freeze_grads: bool, freeze_batchn
     if freeze_dropout:
         layer_components[-1].p = 0
 
-def _set_module_freeze_state(mod: scvi.nn.Encoder|scvi.nn.DecoderSCVI,
+
+def _set_module_freeze_state(mod: scvi.nn.Encoder | scvi.nn.DecoderSCVI,
                              freeeze_gradients: bool,
                              apply_scArches: bool,
                              freeze_batchnorm: bool,
@@ -226,7 +228,7 @@ def _set_module_freeze_state(mod: scvi.nn.Encoder|scvi.nn.DecoderSCVI,
     for child in mod.children():
         if isinstance(child, scvi.nn.FCLayers):
             layers = child.fc_layers
-            if apply_scArches: # only call function if necessary
+            if apply_scArches:  # only call function if necessary
                 child.set_online_update_hooks()
                 _process_layer(layers[0], False, freeze_batchnorm, freeze_dropout)
                 layers = layers[1:]
@@ -234,6 +236,7 @@ def _set_module_freeze_state(mod: scvi.nn.Encoder|scvi.nn.DecoderSCVI,
                 _process_layer(layer, freeeze_gradients, freeze_batchnorm, freeze_dropout)
         else:
             child.requires_grad_(not freeeze_gradients)
+
 
 def _set_params_online_update(
     module: RQMDiva,

scvi/module/_diva.py
@@ -15,12 +15,18 @@ from scvi import REGISTRY_KEYS
 from scvi._types import Tunable
 from scvi.distributions import ZeroInflatedNegativeBinomial, NegativeBinomial, Poisson
 from scvi.module.base import BaseModuleClass, LossOutput, auto_move_data
-from scvi.nn import Encoder, one_hot, MeanOnlyEncoder, RQMDecoder
+from scvi.nn import Encoder, one_hot, MeanOnlyEncoder, RQMDecoder, CholeskyCovEncoder
 from sklearn.utils.class_weight import compute_class_weight
 from torch import nn
-from torch.distributions import Normal, kl_divergence as kl, MixtureSameFamily, Independent, Categorical
+from torch.distributions import Normal, kl_divergence as kl, MultivariateNormal, register_kl
 
 logger = logging.Logger(__name__)
+
+
+@register_kl(Normal, MultivariateNormal)
+def _kl_normal_multivariate_normal(p: Normal, q):
+    p = MultivariateNormal(p.loc, torch.diag_embed(p.stddev ** 2))
+    return kl(p, q)
 
 
 # TODO: support minification
@@ -55,13 +61,14 @@ class DIVA(BaseModuleClass):
         lib_encoder_n_hidden: int = 128,
         lib_encoder_n_layers: int = 1,
         dispersion: Literal["gene", "gene-batch", "gene-label", "gene-cell"] = "gene",
-        log_variational: Tunable[bool] = True,
+        log_variational: Tunable[bool] = True,  # TODO: fix
         gene_likelihood: Literal["zinb", "nb", "poisson"] = "zinb",
         latent_distribution: Literal["normal", "ln"] = "normal",
         use_batch_norm: Literal["encoder", "decoder", "none", "both"] = "both",
         use_layer_norm: Literal["encoder", "decoder", "none", "both"] = "none",
         use_linear_batch_classifier: bool = False,
         use_linear_label_classifier: bool = False,
+        use_prior_based_predictions: bool = False,
         use_size_factor_key: bool = False,
         use_observed_lib_size: Tunable[bool] = True,
         use_learnable_priors: Tunable[bool] = True,
@@ -225,7 +232,7 @@ class DIVA(BaseModuleClass):
         # if latent spaces are used, but the classifiers deactivated, the classifiers are removed from inference to
         # speed up the model
         self._use_batch_classifier = alpha_d != 0
-        self._use_celltype_classifier = alpha_y != 0 and not self._unsupervised
+        self._use_celltype_classifier = alpha_y != 0 and not self._unsupervised and not use_prior_based_predictions
 
         logger.info(f"Using batch classifier:  {self._use_batch_classifier}")
         logger.info(f"Using celltype classifier:  {self._use_celltype_classifier}")
@@ -305,8 +312,8 @@ class DIVA(BaseModuleClass):
             # deactivate the y latent space for unsupervised mode
             if not self._unsupervised:
                 if prior_variance_y is None:
-                    self.prior_zy_y_encoder = Encoder(
-                        n_input=n_labels,
+                    self.prior_zy_y_encoder = CholeskyCovEncoder(
+                        n_input=n_labels - 1,
                         n_output=n_latent_y,
                         n_layers=priors_n_layers,
                         n_hidden=priors_n_hidden,
@@ -319,7 +326,7 @@ class DIVA(BaseModuleClass):
                     )
                 else:
                     self.prior_zy_y_encoder = MeanOnlyEncoder(
-                        n_input=n_labels,
+                        n_input=n_labels - 1,
                         n_output=n_latent_y,
                         n_layers=priors_n_layers,
                         n_hidden=priors_n_hidden,
@@ -384,7 +391,7 @@ class DIVA(BaseModuleClass):
             if self._use_celltype_classifier:
                 self.aux_y_zy_enc = torch.nn.Sequential(
                     *[] if use_linear_label_classifier else [nn.ReLU()],
-                    nn.Linear(n_latent_y, n_labels)
+                    nn.Linear(n_latent_y, n_labels - 1)
                 )
 
         """auxiliary task q_w(d|zd)"""
@@ -523,7 +530,7 @@ class DIVA(BaseModuleClass):
         if self.use_learnable_priors:
             p_zd_d, _ = self.prior_zd_d_encoder(one_hot(d, self.n_batch))
             if not self._unsupervised:
-                p_zy_y, _ = self.prior_zy_y_encoder(one_hot(y, self.n_labels))
+                p_zy_y, _ = self.prior_zy_y_encoder(one_hot(y, self.n_labels - 1))
         else:
             p_zd_d = Normal(torch.zeros_like(zd_x), torch.ones_like(zd_x))
             if not self._unsupervised:
@@ -531,7 +538,13 @@ class DIVA(BaseModuleClass):
 
         # auxiliary losses
         d_hat = self.aux_d_zd_enc(zd_x) if self._use_batch_classifier else None
-        y_hat = self.aux_y_zy_enc(zy_x) if self._use_celltype_classifier else None
+        # """
+        if self._use_celltype_classifier:
+            y_hat = self._pred_cls(zy_x)
+        elif not self._unsupervised:
+            y_hat = self._pred_prior(zy_x)
+        else:
+            y_hat = None
 
         outputs = {'px_recon': px_recon, 'd_hat': d_hat, 'y_hat': y_hat, 'p_zd_d': p_zd_d, 'p_zy_y': p_zy_y}
 
@@ -568,7 +581,7 @@ class DIVA(BaseModuleClass):
         kl_zd = kl(
             inference_outputs["q_zd_x"],
             generative_outputs["p_zd_d"]
-        ).sum(dim=1)
+        ).sum(dim=-1)
 
         if not self.use_observed_lib_size:
             kl_l = kl(
@@ -584,15 +597,15 @@ class DIVA(BaseModuleClass):
                 "No weights initialized for balanced KL of DIVA model.\n"
                 "Use `.init_kl_weights` to initialize them.")
 
-        kl_local_for_warmup = self.beta_d * kl_zd * self._kl_weights_d.to(self.device)[d]
+        kl_local_for_warmup = self.beta_d * kl_zd * self._kl_weights_d.to(self.device)[d].flatten()
 
         if not self._unsupervised:
             kl_zy = kl(
                 inference_outputs["q_zy_x"],
                 generative_outputs["p_zy_y"]
-            ).sum(dim=1)
+            )
 
-            kl_local_for_warmup += self.beta_y * kl_zy * self._kl_weights_y.to(self.device)[y]
+            kl_local_for_warmup += self.beta_y * kl_zy * self._kl_weights_y.to(self.device)[y].flatten()
         else:
             kl_zy = 0
 
@@ -601,12 +614,12 @@ class DIVA(BaseModuleClass):
 
         if self._use_batch_classifier:
             # auxiliary losses
-            aux_d = F.cross_entropy(generative_outputs["d_hat"], d.view(-1, ))
+            aux_d = F.cross_entropy(generative_outputs["d_hat"], d.flatten())
             aux_loss += self.alpha_d * aux_d
             extra_metrics["aux_d"] = aux_d
 
-        if self._use_celltype_classifier:
-            aux_y = F.cross_entropy(generative_outputs["y_hat"], y.view(-1, ), weight=self._ce_weights_y.to(self.device))
+        if not self._unsupervised:
+            aux_y = F.cross_entropy(generative_outputs["y_hat"], y.flatten(), weight=self._ce_weights_y.to(self.device))
             aux_loss += self.alpha_y * aux_y
             extra_metrics["aux_y"] = aux_y
 
@@ -685,7 +698,7 @@ class DIVA(BaseModuleClass):
         if use_mean_as_sample:
             probs = predictor_fn(dist.mean)
         else:
-            pred_avgs = torch.zeros((n_samples, x.shape[0], self.n_labels))
+            pred_avgs = torch.zeros((n_samples, x.shape[0], self.n_labels - 1))
             for i in range(n_samples):
                 y_pred = predictor_fn(dist.sample())
                 pred_avgs[i] = y_pred
@@ -729,17 +742,14 @@ class DIVA(BaseModuleClass):
         return probs.gather(1, y.unsqueeze(1)).squeeze(1).cpu().numpy(), y.cpu().numpy()
 
     def _pred_prior(self, zy_x: torch.Tensor) -> torch.Tensor:
-        encodings = torch.eye(self.n_labels, device=self.device)
-        probs = torch.zeros((zy_x.shape[0], self.n_labels), device=self.device)
-        for idx in range(self.n_labels):
-            p_zy_y: torch.distributions.Normal
-            p_zy_y, _ = self.prior_zy_y_encoder(encodings[idx:idx + 1, :])
-            ind = Independent(p_zy_y, 1)
-            probs[:, idx] = ind.expand([zy_x.shape[0]]).log_prob(zy_x)
+        encodings = torch.eye(self.n_labels - 1, device=self.device)
+        p_zy_y: torch.distributions.MultivariateNormal
+        p_zy_y, _ = self.prior_zy_y_encoder(encodings)
+        probs = p_zy_y.log_prob(zy_x.unsqueeze(1))
         return F.softmax(probs, dim=-1)
 
     def _pred_cls(self, zy_x: torch.Tensor) -> torch.Tensor:
-        return torch.softmax(self.aux_y_zy_enc(zy_x), dim=-1)
+        return F.softmax(self.aux_y_zy_enc(zy_x), dim=-1)
 
     def init_ce_weight_y(self, adata: AnnData, indices, label_key: str):
         """
@@ -754,13 +764,13 @@ class DIVA(BaseModuleClass):
         """
         cat_y = adata.obs[label_key].cat.codes[indices]
         data_index_space = np.unique(cat_y)
-        model_index_space = np.array(range(self.n_labels))
+        model_index_space = np.array(range(self.n_labels - 1))
 
         if len(t := np.setdiff1d(data_index_space, model_index_space)) > 0:
             raise ValueError('Data contains classes with indices {}. Those are not supported by the used model instance. Make sure you only'
                              'use data that was set up for the model instance.'.format(t))
 
-        weight_arr = np.zeros(self.n_labels)
+        weight_arr = np.zeros(self.n_labels - 1)
         weight_arr[data_index_space] = compute_class_weight('balanced', classes=data_index_space, y=cat_y)
 
         self._ce_weights_y = torch.tensor(weight_arr, device=self.device, dtype=torch.float)
@@ -781,26 +791,14 @@ class DIVA(BaseModuleClass):
 
         """use # samples / (1 + # classes * # bincount) to allow calculation for empty batches (here 1+ in divisor)"""
         cat_d_corrected = np.append(cat_d, range(self.n_batch))
-        cat_y_corrected = np.append(cat_y, range(self.n_labels))
+        cat_y_corrected = np.append(cat_y, range(self.n_labels - 1))
 
-        self._kl_weights_y = torch.from_numpy(compute_class_weight('balanced', classes=np.array(range(self.n_labels)), y=cat_y_corrected))
+        self._kl_weights_y = torch.from_numpy(
+            compute_class_weight('balanced', classes=np.array(range(self.n_labels - 1)), y=cat_y_corrected))
         self._kl_weights_d = torch.from_numpy(compute_class_weight('balanced', classes=np.array(range(self.n_batch)), y=cat_d_corrected))
 
         self._kl_weights_y[cat_y_holdout] = 1
         self._kl_weights_d[cat_d_holdout] = 1
-
-    @torch.inference_mode()
-    def full_y_prior_dist(self):
-        """
-        Combines the learned priors distribution to a joint gaussian mixture model.
-        Returns
-        -------
-        MixtureSameFamily
-            The gaussian mixture model combining the learned priors of the cell-type latent space.
-        """
-        encodings = torch.eye(self.n_labels, device=self.device)
-        p_zy_y, _ = self.prior_zy_y_encoder(encodings)
-        return MixtureSameFamily(Categorical(torch.ones(self.n_labels, device=self.device)), Independent(p_zy_y, 1))
 
     @torch.inference_mode()
     def sample_log_likelihood(self, tensors, n_samples: int):

scvi/module/_diva_rqm.py
@@ -7,19 +7,19 @@ from typing import Literal, Callable
 import numpy as np
 import torch
 import torch.nn.functional as F
-from scipy.stats import entropy
 from scvi import REGISTRY_KEYS
 from scvi.distributions import ZeroInflatedNegativeBinomial, NegativeBinomial, Poisson
 from scvi.module._diva import DIVA
 from scvi.module.base import LossOutput, auto_move_data
 from scvi.nn import one_hot
-from torch.distributions import kl_divergence as kl, OneHotCategorical, Independent, Normal
+from torch.distributions import kl_divergence as kl, Normal
 
 logger = logging.getLogger(__name__)
 
-class RQMDiva(DIVA):
 
+class RQMDiva(DIVA):
     _pred_func: Callable[[torch.Tensor], torch.Tensor]
+
     def __init__(self, *args,
                  label_generator: Literal['prior_based', 'internal_classifier'],
                  conservativeness: float,
@@ -37,7 +37,7 @@ class RQMDiva(DIVA):
         super().__init__(*args, **kwargs)
         if conservativeness > 1 or conservativeness < 0:
             raise ValueError("conservativeness must be a value between 0 and 1")
-        if conservativeness in [0,1]:
+        if conservativeness in [0, 1]:
             logger.warn(f"conservativeness is: {conservativeness}. Hence, not all training samples might be considered, potentially "
                         f"affecting the batch size. It is better to remove unwanted training samples from training data beforehand.")
 
@@ -62,7 +62,7 @@ class RQMDiva(DIVA):
         library: torch.Tensor,
         size_factor=None,
     ) -> dict[str, torch.Tensor | torch.distributions.Distribution]:
-        has_no_label = (y == (self.n_labels-1)).flatten()
+        has_no_label = (y == (self.n_labels - 1)).flatten()
         if not self.use_size_factor_key:
             size_factor = library
 
@@ -103,9 +103,9 @@ class RQMDiva(DIVA):
         # generate pseudo labels for samples without labels
         zy_x_unlabeled = zy_x[has_no_label]
         with torch.no_grad():
-            self.eval() # TODO: analyze impact
+            self.eval()  # TODO: analyze impact
             probs = self._pred_func(zy_x_unlabeled)
-            self.train() # TODO: analyze impact
+            self.train()  # TODO: analyze impact
         p_y_zy = torch.distributions.Categorical(probs)
         y_pseudo = probs.argmax(dim=-1).view(-1, 1)
         marginal_pseudo_weight[has_no_label] = torch.exp(p_y_zy.log_prob(y_pseudo.flatten()).view(-1, ))
@@ -116,20 +116,19 @@ class RQMDiva(DIVA):
         if self.use_learnable_priors:
             p_zd_d, _ = self.prior_zd_d_encoder(one_hot(d, self.n_batch))
             if not self._unsupervised:
-                p_zy_y, _ = self.prior_zy_y_encoder(one_hot(y, self.n_labels))
+                p_zy_y, _ = self.prior_zy_y_encoder(one_hot(y, self.n_labels - 1))
         else:
             p_zd_d = Normal(torch.zeros_like(zd_x), torch.ones_like(zd_x))
             if not self._unsupervised:
                 p_zy_y = Normal(torch.zeros_like(zy_x), torch.ones_like(zy_x))
 
-        return {'px_recon': px_recon, 'p_zd_d': p_zd_d, 'p_zy_y': p_zy_y, 'marginal_pseudo_weight': marginal_pseudo_weight, 'has_no_label': has_no_label}
-
+        return {'px_recon': px_recon, 'p_zd_d': p_zd_d, 'p_zy_y': p_zy_y, 'marginal_pseudo_weight': marginal_pseudo_weight,
+                'has_no_label': has_no_label}
 
     def _weight_conservativeness(self, tensor: torch.Tensor, has_no_label: torch.Tensor) -> torch.Tensor:
-        mult_vec = torch.ones_like(tensor)*self.conservativeness
-        mult_vec[has_no_label] = (1-self.conservativeness)
+        mult_vec = torch.ones_like(tensor) * self.conservativeness
+        mult_vec[has_no_label] = (1 - self.conservativeness)
         return tensor * mult_vec
-
 
     def loss(
         self,
@@ -150,7 +149,7 @@ class RQMDiva(DIVA):
         kl_zd = kl(
             inference_outputs['q_zd_x'],
             generative_outputs['p_zd_d'],
-        ).sum(dim=1)
+        ).sum(dim=-1)
         # kl_zd = self._weight_conservativeness(kl_zd, has_no_label)
 
         if not self.use_observed_lib_size:
@@ -164,8 +163,9 @@ class RQMDiva(DIVA):
 
         marginal_pseudo_weight = generative_outputs['marginal_pseudo_weight']
 
-        kl_zy = marginal_pseudo_weight * (inference_outputs['q_zy_x'].log_prob(inference_outputs['zy_x']) - generative_outputs['p_zy_y'].log_prob(
-            inference_outputs['zy_x'])).sum(-1).view(-1,)
+        kl_zy = marginal_pseudo_weight * (
+                inference_outputs['q_zy_x'].log_prob(inference_outputs['zy_x']).sum(-1) - generative_outputs['p_zy_y'].log_prob(
+                inference_outputs['zy_x']))
         # kl_zy = self._weight_conservativeness(kl_zy, has_no_label)
 
         # KL loss
@@ -203,10 +203,10 @@ class RQMDiva(DIVA):
 
     @torch.inference_mode()
     def sample_reconstruction_ll(
-            self,
-            tensors,
-            n_mc_samples,
-            n_mc_samples_per_pass=1,
+        self,
+        tensors,
+        n_mc_samples,
+        n_mc_samples_per_pass=1,
     ):
         """Computes the marginal log likelihood of the model.
 

scvi/nn/_base_components.py
@@ -3,8 +3,9 @@ from collections.abc import Iterable
 from typing import Callable, Literal, Optional
 
 import torch
+import torch.nn.functional as F
 from torch import nn
-from torch.distributions import Normal
+from torch.distributions import Normal, MultivariateNormal
 from torch.nn import ModuleList
 
 from ._utils import one_hot
@@ -186,8 +187,10 @@ class Predecoder(nn.Module):
     def __init__(self, *args, **kwargs):
         super().__init__()
         self.predecoder = FCLayers(*args, **kwargs)
+
     def forward(self, x):
         return self.predecoder(x)
+
 
 # Encoder
 class Encoder(nn.Module):
@@ -291,6 +294,90 @@ class Encoder(nn.Module):
         if self.return_dist:
             return dist, latent
         return q_m, q_v, latent
+
+
+class CholeskyCovEncoder(nn.Module):
+    def __init__(
+        self,
+        n_input: int,
+        n_output: int,
+        n_cat_list: Iterable[int] = None,
+        n_layers: int = 1,
+        n_hidden: int = 128,
+        dropout_rate: float = 0.1,
+        distribution: str = "normal",
+        var_eps: float = 1e-4,
+        var_activation_diag: Optional[Callable] = None,
+        var_activation_off_diag: Optional[Callable] = None,
+        return_dist: bool = False,
+        **kwargs,
+    ):
+        super().__init__()
+
+        self.distribution = distribution
+        self.var_eps = var_eps
+        self.encoder = FCLayers(
+            n_in=n_input,
+            n_out=n_hidden,
+            n_cat_list=n_cat_list,
+            n_layers=n_layers,
+            n_hidden=n_hidden,
+            dropout_rate=dropout_rate,
+            **kwargs,
+        )
+        self.mean_encoder = nn.Linear(n_hidden, n_output)
+        self.var_encoder = nn.Sequential(
+            # nn.BatchNorm1d(n_hidden), # TODO: potential bug source
+            nn.Linear(n_hidden, (n_output * (n_output + 1)) // 2)
+        )
+        self.return_dist = return_dist
+
+        if distribution == "ln":
+            self.z_transformation = nn.Softmax(dim=-1)
+        else:
+            self.z_transformation = _identity
+        self.var_activation_diag = F.softplus if var_activation_diag is None else var_activation_diag
+        self.var_activation_off_diag = torch.exp if var_activation_off_diag is None else var_activation_off_diag
+
+    def forward(self, x: torch.Tensor, *cat_list: int):
+        r"""The forward computation for a single sample.
+
+         #. Encodes the data into latent space using the encoder network
+         #. Generates a mean \\( q_m \\) and full covariance matrix variance \\( \\Sigma \\)
+         #. Samples a new value from an multivariate normal \\( \\sim Ne(q_m, \\Sigma \\)
+
+        Parameters
+        ----------
+        x
+            tensor with shape (n_input,)
+        cat_list
+            list of category membership(s) for this sample
+
+        Returns
+        -------
+        3-tuple of :py:class:`torch.Tensor`
+            tensors of shape ``(n_latent,)`` for mean and var, and sample
+
+        """
+        # Parameters for latent distribution
+        q = self.encoder(x, *cat_list)
+        q_m = self.mean_encoder(q)
+        d = q_m.shape[1]
+        l_chol = torch.zeros((q_m.shape[0], d, d), device=q_m.device)
+        l_chol[:, *torch.tril_indices(d, d, offset=0).tolist()] = self.var_encoder(q)
+        range_idx = torch.arange(d)
+        l_chol[:, range_idx, range_idx] = self.var_activation_diag(l_chol[:, range_idx, range_idx])
+
+        cov_mats = l_chol @ l_chol.transpose(-1, -2)
+        cov_mats[:, range_idx, range_idx] += self.var_eps
+        try:
+            dist = MultivariateNormal(q_m, cov_mats)
+        except:
+            print("NO")
+        latent = self.z_transformation(dist.rsample())
+        if self.return_dist:
+            return dist, latent
+        return q_m, cov_mats, latent
 
 
 class MeanOnlyEncoder(nn.Module):