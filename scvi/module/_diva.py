fix softmax

scvi/module/_diva.py
@@ -745,11 +745,10 @@ class DIVA(BaseModuleClass):
         encodings = torch.eye(self.n_labels - 1, device=self.device)
         p_zy_y: torch.distributions.MultivariateNormal
         p_zy_y, _ = self.prior_zy_y_encoder(encodings)
-        probs = p_zy_y.log_prob(zy_x.unsqueeze(1))
-        return F.softmax(probs, dim=-1)
+        return p_zy_y.log_prob(zy_x.unsqueeze(1))
 
     def _pred_cls(self, zy_x: torch.Tensor) -> torch.Tensor:
-        return F.softmax(self.aux_y_zy_enc(zy_x), dim=-1)
+        return self.aux_y_zy_enc(zy_x)
 
     def init_ce_weight_y(self, adata: AnnData, indices, label_key: str):
         """

scvi/module/_diva_rqm.py
@@ -104,7 +104,7 @@ class RQMDiva(DIVA):
         zy_x_unlabeled = zy_x[has_no_label]
         with torch.no_grad():
             self.eval()  # TODO: analyze impact
-            probs = self._pred_func(zy_x_unlabeled)
+            probs = F.softmax(self._pred_func(zy_x_unlabeled), dim=-1)
             self.train()  # TODO: analyze impact
         p_y_zy = torch.distributions.Categorical(probs)
         y_pseudo = probs.argmax(dim=-1).view(-1, 1)